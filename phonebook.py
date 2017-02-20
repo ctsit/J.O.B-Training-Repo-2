587b68cb9efc6cb95e65c65ecde42a73a97853df
Akash Agarwal <agarwalakash@ctsitadmins-MacBook-Pro.local>
2017-02-20 16:38:52 -0500

    If this commit is applied I will be able to sort the phonebook by last name

--- phonebook.py
+++ phonebook.py
@@ -5,6 +5,12 @@ import csv
 #Open phonebook.csv file
 myfile = csv.reader(open("phonebook.csv", 'rU'), delimiter=",", dialect=csv.excel_tab)
 
+contlist=[]
 #Return information from file
 for first_name, last_name, phone1 in myfile:
-    print first_name
+    contlist.append((first_name, last_name, phone1))
+
+sortedlist = sorted(contlist, key=lambda tup: tup[1])
+sortedlist = sortedlist[:-1]
+with open('sorted.txt', 'w') as fp:
+    fp.write('\n'.join('%s %s %s' % x for x in sortedlist))
