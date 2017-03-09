ce464b0ff07b051b6de614b2bcb9e7140f6e1a37
Kevin Hanson <hansonks@gmail.com>
2017-03-09 16:43:42 -0500

    Update phonebook.py

--- phonebook.py
+++ phonebook.py
@@ -1,16 +1,10 @@
-#Import Python modules for handling csv
-import sys
-import csv
-
-#Open phonebook.csv file
-myfile = csv.reader(open("phonebook.csv", 'rU'), delimiter=",", dialect=csv.excel_tab)
-
-contlist=[]
-#Return information from file
-for first_name, last_name, phone1 in myfile:
-    contlist.append((first_name, last_name, phone1))
-
-sortedlist = sorted(contlist, key=lambda tup: tup[1])
-sortedlist = sortedlist[:-1]
-with open('sorted.txt', 'w') as fp:
-    fp.write('\n'.join('%s %s %s' % x for x in sortedlist))
++#Import Python modules for handling csv
+ +import sys
+ +import csv
+ +
+ +#Open phonebook.csv file
+ +myfile = csv.reader(open("phonebook.csv", 'rU'), delimiter=",", dialect=csv.excel_tab)
+ +
+ +#Return information from file
+ +for first_name, last_name, phone1 in myfile:
+ +    print first_name 
