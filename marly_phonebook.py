32a793f36a6068cd6a90e5bf0a99be4c08041376
marlycormar <marlycormar@ufl.edu>
2017-06-16 10:41:26 -0400

    Add new version of function phonebook.py

---
+++ marly_phonebook.py
@@ -0,0 +1,15 @@
+#Import Python modules for handling csv
+import sys
+import csv
+import operator
+#Open phonebook.csv file
+with open("phonebook.csv", "rU") as myfile:
+    checkreader = csv.reader(myfile)
+    header = next(checkreader)
+    sorted_data = sorted(checkreader, key = operator.itemgetter(1))
+with open("marly_newphonebook.csv", "wb") as my_file:
+    fileWriter = csv.writer(my_file, delimiter=',')
+    if header:
+        fileWriter.writerow(header)
+    for row in sorted_data:
+        fileWriter.writerow(row)
