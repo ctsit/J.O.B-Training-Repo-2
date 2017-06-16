e84d0d2fa80d3090938314ca7062d5f794a526aa
prasadlan <prasad2311@gmail.com>
2017-06-16 10:12:18 -0400

    created a sorted phonebook based on last names

--- phonebook.py
+++ phonebook.py
@@ -7,9 +7,11 @@ with open("phonebook.csv", "rU") as myfile:
     checkreader = csv.reader(myfile)
     header = next(checkreader)
     sorted_data = sorted(checkreader, key = operator.itemgetter(1))
-with open("newphonebook.csv", "wb") as my_file:
+with open("prasadphonebook.csv", "wb") as my_file:
     fileWriter = csv.writer(my_file, delimiter=',')
     if header:
         fileWriter.writerow(header)
     for row in sorted_data:
         fileWriter.writerow(row)
+#File sorted based on last names
+
