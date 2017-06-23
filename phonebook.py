b8a03d867542bca0f909f8c3d4bda8a5211cd384
suryayalla <prasannakumar006@gmail.com>
2017-06-23 14:37:35 -0400

    Revert "sort the phonebook by last names and output to a file"
    This reverts commit 65cd62da2a08a93797dbfbb17e51d5419f456079.

--- phonebook.py
+++ phonebook.py
@@ -1,33 +1,10 @@
-#Import Python modules for handling csv
-import sys
-import csv
-
-#Open phonebook.csv file
-myfile = csv.reader(open("phonebook.csv", 'rU'), delimiter=",", dialect=csv.excel_tab)
-
-header=myfile.next()
-print(header)
-
-class info():
-	def __init__(self, first_name, last_name, phone1):
-		self.first_name = first_name
-		self.last_name = last_name
-		self.phone1 = phone1
-	def __cmp__(self,other):
-		return cmp(self.last_name,other.last_name)
-
-info_list = []
-#Return information from file
-for first_name, last_name, phone1 in myfile:
-    info_list.append(info(first_name, last_name, phone1));
-
-info_list.sort()
-for i in info_list:
-	print(i.last_name);
-
-with open('outfile.csv', 'wb') as csvfile:
-	writer = csv.writer(csvfile)
-	writer.writerow(header)
-	for info in info_list:
-		writer.writerow([info.first_name, info.last_name, info.phone1])
-	writer.truncate(csvfile.tell()-1)
++#Import Python modules for handling csv
+ +import sys
+ +import csv
+ +
+ +#Open phonebook.csv file
+ +myfile = csv.reader(open("phonebook.csv", 'rU'), delimiter=",", dialect=csv.excel_tab)
+ +
+ +#Return information from file
+ +for first_name, last_name, phone1 in myfile:
+ +    print first_name 
