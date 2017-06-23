b46c63b032e6edf9af77fdc6e3ef3d4ab857e10c
suryayalla <prasannakumar006@gmail.com>
2017-06-23 14:45:21 -0400

    sort phonebook by last name and create newphonebook.csv

--- phonebook.py
+++ phonebook.py
@@ -1,15 +1,29 @@
 #Import Python modules for handling csv
 import sys
 import csv
-import operator
+
 #Open phonebook.csv file
-with open("phonebook.csv", "rU") as myfile:
-    checkreader = csv.reader(myfile)
-    header = next(checkreader)
-    sorted_data = sorted(checkreader, key = operator.itemgetter(1))
-with open("newphonebook.csv", "wb") as my_file:
-    fileWriter = csv.writer(my_file, delimiter=',')
-    if header:
-        fileWriter.writerow(header)
-    for row in sorted_data:
-        fileWriter.writerow(row)
+myfile = csv.reader(open("phonebook.csv", 'rU'), delimiter=",", dialect=csv.excel_tab)
+
+header=myfile.next()
+
+class info():
+	def __init__(self, first_name, last_name, phone1):
+		self.first_name = first_name
+		self.last_name = last_name
+		self.phone1 = phone1
+	def __cmp__(self,other):
+		return cmp(self.last_name,other.last_name)
+
+info_list = []
+#Return information from file
+for first_name, last_name, phone1 in myfile:
+    info_list.append(info(first_name, last_name, phone1));
+
+info_list.sort()
+
+with open('newphonebook.csv', 'wb') as csvfile:
+	writer = csv.writer(csvfile)
+	writer.writerow(header)
+	for info in info_list:
+		writer.writerow([info.first_name, info.last_name, info.phone1])
