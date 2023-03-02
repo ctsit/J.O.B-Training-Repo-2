69b73428f13a991d24a5009cbfd08995053f33dc
B. Henderson <henderson.b@ufl.edu>
2023-03-02 14:16:40 -0500

    get pandas on the job

--- phonebook.py
+++ phonebook.py
@@ -1,18 +1,2 @@
-#Import Python modules for handling csv
-import sys
-import csv
-import operator
-#Marly was here
-#Amber was here
-#Toby was here
-#Open phonebook.csv file
-with open("phonebook.csv", "rU") as myfile:
-    checkreader = csv.reader(myfile)
-    header = next(checkreader)
-    sorted_data = sorted(checkreader, key = operator.itemgetter(0))
-with open("newphonebook.csv", "wb") as my_file:
-    fileWriter = csv.writer(my_file, delimiter=',')
-    if header:
-        fileWriter.writerow(header)
-    for row in sorted_data:
-        fileWriter.writerow(row)
+import pandas as pd
+
